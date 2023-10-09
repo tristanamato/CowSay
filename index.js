6561ccb7ddaffb44fc621d63e04a221e0680002e
Tristan AMATO <tristan.amato@edu.esce.fr>
2023-10-09 14:23:41 +0200

    petit update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const myInformation = require('./information.js');
-const cowsay = require('cowsay 
+const cowsay = require('cowsay');
 const message = `My name is ${myInformation.name}  from ${myInformation.campus}`;
 const cowMessage = cowsay.say({text: message});
 
